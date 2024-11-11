d3f71a11f0d25521703e3dd9ca616e80143dfddc
Fazylov Mihail <161601509+fazylovMihail@users.noreply.github.com>
2024-11-11 23:46:50 +0500

    Создание контента
    html css вёрстка и 1/2 работы над кнопками в js

--- js/script.js
+++ js/script.js
@@ -1,41 +1,84 @@
 // селекторы, классы и айди
 const selectsBtnSelector = '.selects';
-const selectclass = 'select-btn';
+const selectClassHeader = 'select-btn-header';
 const headerWrapperSelector = '.header-wrapper';
+const contentTextConteinerSelector = '.content-text-conteiner';
+const circleBtnSelector = '.circle-btn';
+const selectClassContent = 'select-btn-content';
 
 // переменные
 const headerSelectsBtn = document.querySelectorAll(selectsBtnSelector);
 const headerWrapper = document.querySelector(headerWrapperSelector);
+
 const aboutTextHeader = [
     '<div class="header-wrapper-downoald header-text">Этот проект был создан с целью упростить поиск формул по физике<br><span class="wrapper-smile">:)</span></div>',
     '<div class="header-wrapper-downoald"><div class="header-wrapper-downoald-btn">Скачать для андройд</div></div>',
 ];
 
 // код для работы header
-function Select(element){
-    element.classList.add(selectclass);
+function Select(element, addClass){
+    element.classList.add(addClass);
 }
-Select(headerSelectsBtn[0]);
-function Remove(element){
-    element.classList.remove(selectclass);
+Select(headerSelectsBtn[0], selectClassHeader);
+function Remove(element, removeClass){
+    element.classList.remove(removeClass);
 }
-function SelectManager(element, index){
-    if(!element.classList.contains('select-btn')){
-        Select(element);
-
+function SelectManagerHeader(element, index, workClass){
+    if(!element.classList.contains(workClass)){
+        Select(element, workClass);
+        
         if(index<=0){
-            Remove(headerSelectsBtn[index+1]);
+            Remove(headerSelectsBtn[index+1], workClass);
         }
         else{
-            Remove(headerSelectsBtn[index-1]);
+            Remove(headerSelectsBtn[index-1], workClass);
         }
     }
     headerWrapper.innerHTML = aboutTextHeader[index];
 }
-SelectManager(headerSelectsBtn[0],0);
+SelectManagerHeader(headerSelectsBtn[0],0,selectClassHeader);
 
 for(let i = 0;i<headerSelectsBtn.length;i++){
     headerSelectsBtn[i].addEventListener('click', ()=>{
-        SelectManager(headerSelectsBtn[i], i);
+        SelectManagerHeader(headerSelectsBtn[i], i, selectClassHeader);
+    })
+}
+
+// переменные
+const contentTextConteiner = document.querySelector(contentTextConteinerSelector);
+const circleBtn = document.querySelectorAll(circleBtnSelector);
+
+const contentText = [
+    '7 класс',
+    '8 класс',
+    '9 класс',
+]
+
+// код для работы контейнера с контентом
+
+function SelectManagerContent(element, index, workClass){
+    if(!element.classList.contains(workClass)){
+        Select(element, workClass);
+
+        if(index<=0){
+            Remove(circleBtn[index+1], workClass);
+            Remove(circleBtn[index+2], workClass);
+        }
+        else if(index==1){
+            Remove(circleBtn[index-1], workClass);
+            Remove(circleBtn[index+1], workClass);
+        }
+        else{
+            Remove(circleBtn[index-2], workClass);
+            Remove(circleBtn[index-1], workClass);
+        }
+    }
+    contentTextConteiner.innerHTML = contentText[index];
+}
+SelectManagerContent(circleBtn[0], 0, selectClassContent);
+
+for(let i = 0;i<circleBtn.length;i++){
+    circleBtn[i].addEventListener('click', ()=>{
+        SelectManagerContent(circleBtn[i], i, selectClassContent);
     })
 }
