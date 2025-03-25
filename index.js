e27a003918d05b5ec83651a710db839872dd11c0
Fazylov Mihail <161601509+fazylovMihail@users.noreply.github.com>
2025-03-25 19:54:33 +0500

    default

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { get_cookie } from "./source/js/system/cookie.js";
 const index = get_cookie('Class_Name'),
 circle_btn = document.querySelectorAll('.circle_btn'), select_class_str = 'select_circle_btn';
 
-(index)? circle_btn_manager(circle_btn[index], circle_btn, select_class_str, index): circle_btn_manager(circle_btn[0], circle_btn, select_class_str, 0);;
+(index)? circle_btn_manager(circle_btn[index], circle_btn, select_class_str, index): circle_btn_manager(circle_btn[0], circle_btn, select_class_str, 0);
 for(let i=0;i<circle_btn.length;i++){
     circle_btn[i].addEventListener('click', () => {circle_btn_manager(circle_btn[i], circle_btn, select_class_str, i)})
 }
