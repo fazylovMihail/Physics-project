9dddc9f9223b01a3ff8b52c541011e8e310feae4
Fazylov Mihail <161601509+fazylovMihail@users.noreply.github.com>
2025-03-25 19:52:31 +0500

    Оптимизация под работы с cookie

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import { circle_btn_manager } from "./source/js/frontend/front.js";
+import { get_cookie } from "./source/js/system/cookie.js";
 
-const circle_btn = document.querySelectorAll('.circle_btn'), select_class_str = 'select_circle_btn';
-circle_btn_manager(circle_btn[0], circle_btn, select_class_str, 0)
+const index = get_cookie('Class_Name'),
+circle_btn = document.querySelectorAll('.circle_btn'), select_class_str = 'select_circle_btn';
+
+(index)? circle_btn_manager(circle_btn[index], circle_btn, select_class_str, index): circle_btn_manager(circle_btn[0], circle_btn, select_class_str, 0);;
 for(let i=0;i<circle_btn.length;i++){
     circle_btn[i].addEventListener('click', () => {circle_btn_manager(circle_btn[i], circle_btn, select_class_str, i)})
 }
